866c081ed12cffe568c4165462921dbfd0f019dd
Bastien Faivre <faivre.bast@gmail.com>
2022-03-03 22:17:03 +0100

    feat: issue #10 algorithm implemented

--- algorithms/dp/bitmasking.py
+++ algorithms/dp/bitmasking.py
@@ -1,10 +1,13 @@
+from collections import defaultdict
+
 MAX_NB_PEOPLE = 10
 
 """
 Check that the given caps sets is valid 
 
 :param capsSets: The caps sets to check
-:returns: nothing but raises errors if invalid caps sets
+:param nbCaps: The number of caps
+:returns: Nothing but raises errors if invalid caps sets
 """
 def checkArgument(capsSets: list, nbCaps: int):
     if len(capsSets) > MAX_NB_PEOPLE:
@@ -18,17 +21,71 @@ def checkArgument(capsSets: list, nbCaps: int):
             if cap > nbCaps:
                 raise ValueError(f'The maximum cap id is {nbCaps}, but you gave a cap id "{cap}" in caps set {capsSet} !')
 
+"""
+Initialize the variables
+
+:param capsSets: The caps sets given
+:param nbCaps: The number of caps
+:returns: The number of people, the caps dict and the DP matrix
+"""
 def initialization(capsSets: list, nbCaps: int):
 
+    # check if the arguments are valid
     checkArgument(capsSets, nbCaps)
 
     nbPeople = len(capsSets)
     nbMasks = 2 ** nbPeople
 
-    # TODO initialize the caps dict
-    # TODO initialize the DP matrix
+    # map each cap to its list of persons that have it
+    caps = defaultdict(list)
+    for person in range(nbPeople):
+        for cap_id in capsSets[person]:
+            caps[cap_id].append(person)
 
-    # TODO return the values
+    # initialize the DP matrix
+    DP = [[-1 for cap_id in range(nbCaps + 1)] for mask in range(nbMasks)]
+
+    return nbPeople, caps, DP
+
+"""
+Count the number of satisfying arrangement from cap_id to last cap
+
+:param nbPeople: The number of people
+:param nbCaps: The number of caps
+:param caps: The caps dict mapping each cap to its list of person that have it
+:param DP: The DP matrix
+:param mask: The current mask
+:param cap_id: The current cap
+:returns: The number of unique ways to assign unique caps
+"""
+def countUniqueWaysFrom(nbPeople, nbCaps, caps, DP, mask, cap_id):
+    
+    # if the mask is full, then we found a satisfying arrangement
+    if mask == (1 << nbPeople) - 1:
+        return 1
+
+    # if there are no more cap, then we did not found a satisfying arrangement
+    if cap_id > nbCaps:
+        return 0
+
+    # if this subproblem is already computed, just return the solution
+    if DP[mask][cap_id] != -1:
+        return DP[mask][cap_id]
+
+    # count the unique ways without taking current cap into account
+    nbUniqueWays = countUniqueWaysFrom(nbPeople, nbCaps, caps, DP, mask, cap_id + 1)
+
+    if cap_id in caps:
+        for person in caps[cap_id]:
+            # check if the person is not already wearing a cap
+            if not (mask & (1 << person)):
+                # count the unique ways if the person wears the current cap
+                nbUniqueWays += countUniqueWaysFrom(nbPeople, nbCaps, caps, DP, mask | (1 << person), cap_id + 1)
+
+    # store the result in the DP matrix
+    DP[mask][cap_id] = nbUniqueWays
+
+    return DP[mask][cap_id]
 
 """
 Determines the nr of ways to assign unique caps to every person using bitmasking and dynamic programming (dp).
@@ -36,19 +93,17 @@ Determines the nr of ways to assign unique caps to every person using bitmasking
 :param capSets: A nested list containing the caps of every person. 
 :returns: The nr of ways to select unique caps for every person.
 """
-def assignUniqueCaps(capsSets: list, nbCaps: int) -> int:
+def countUniqueWays(capsSets: list, nbCaps: int) -> int:
 
-    # TODO assign values from initialization
-
-    # TODO call the algorithm
+    nbPeople, caps, DP = initialization(capsSets, nbCaps)
     
-    return 0
+    return countUniqueWaysFrom(nbPeople, nbCaps, caps, DP, 0, 1)
 
 # For debugging purposes
 def main():
-    caps=[[1,2,3], [4], [1,2]]
+    caps = [[1,2,3], [4], [1,2]]
     nbCaps = 4
-    print(assignUniqueCaps(caps, nbCaps))
+    print(countUniqueWays(caps, nbCaps))
     
 if __name__ == '__main__':
     main()
